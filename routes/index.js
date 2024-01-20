89bc41a6efd541b94d751ddb3550d4043cb64245
Suman <sumankamilya27@gmail.com>
2024-01-21 01:21:11 +0530

    created profile, register and login page with necessary routes

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,47 @@
 var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
-const postModel = require("./posts");
-const localStrategy = require("passport-local");
 const passport = require("passport");
-const upload = require("./multer");
+const localStrategy = require("passport-local");
 
 passport.use(new localStrategy(userModel.authenticate()));
 
-router.get("/", function (req, res, next) {
+router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/login", (req, res) => {
-  res.render("login", { error: req.flash("error") });
+router.get("/register", (req, res) => {
+  res.render("register");
 });
 
-router.get("/profile", isLoggedIn, async (req, res, next) => {
-  const user = await userModel.findOne({
-    username: req.session.passport.user,
-  })
-  .populate("posts")
-  console.log(user);
-  res.render("profile", { user });
-});
-
-router.get("/feed", (req, res) => {
-  res.render("feed");
-});
-
-router.post(
-  "/upload",
-  isLoggedIn,
-  upload.single("file"),
-  async (req, res, next) => {
-    if (!req.file) {
-      return res.status(404).send("no file were given");
-    }
-    const user = await userModel.findOne({
-      username: req.session.passport.user,
-    });
-    const post = await postModel.create({
-      image: req.file.filename,
-      imageText: req.body.filecaption,
-      user: user._id,
-    });
-    user.posts.push(post._id);
-    await user.save();
-    res.redirect('/profile');
-  }
-);
-
-router.post("/register", async (req, res) => {
-  const { username, email, fullname } = req.body;
-  const userData = new userModel({
-    username,
-    email,
-    fullname,
+router.post("/register", (req, res) => {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact,
   });
 
-  userModel.register(userData, req.body.password).then(() => {
+  userModel.register(data, req.body.password).then(() => {
     passport.authenticate("local")(req, res, () => {
       res.redirect("/profile");
     });
   });
 });
 
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile");
+});
+
 router.post(
   "/login",
   passport.authenticate("local", {
+    failureRedirect: "/",
     successRedirect: "/profile",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
+  }),
+  (req, res, next) => {}
 );
 
-router.get("/logout", (req, res) => {
+router.get("/logout", (req, res, next) => {
   req.logout((err) => {
     if (err) {
       return next(err);
@@ -85,8 +51,10 @@ router.get("/logout", (req, res) => {
 });
 
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
 }
 
 module.exports = router;
